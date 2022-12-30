01a3e4644b2361a9ee361509c960afa2ac1856ef
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2022-12-30 18:06:32 +0000

    added 404 page

--- js/item/item.js
+++ js/item/item.js
@@ -6,4 +6,7 @@ const itemName = urlParams.get("title");
 
 const itemData = catalogData.find((item) => item.title === itemName);
 
+// if item not found, redirect to 404 page
+if (!itemData) window.location.href = "../html/404.html";
+
 console.log(itemData); // Objeto com os dados do item/jogo
