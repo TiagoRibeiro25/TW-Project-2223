cd0442bd23d96a2f05d360c7166c61538592999a
JoseNogueira13 <92999470+JoseNogueira13@users.noreply.github.com>
2023-01-14 15:16:38 +0000

    html completo/ css Em processo

--- js/item/item.js
+++ js/item/item.js
@@ -1,4 +1,6 @@
+import { addEventListenerToBtns } from "../catalog/addToCart.js";
 import { catalogData } from "../getData.js";
+
 
 // get item name from the url
 const urlParams = new URLSearchParams(window.location.search);
@@ -10,3 +12,20 @@ const itemData = catalogData.find((item) => item.title === itemName);
 if (!itemData) window.location.href = "../html/404.html";
 
 console.log(itemData); // Objeto com os dados do item/jogo
+document.querySelector("h1").innerHTML = itemData.title
+document.querySelector("img").src = itemData.image
+document.querySelector(".gameDescription").innerHTML = itemData.description
+document.querySelector(".gameDetails").innerHTML += `
+    <p class="gamePrice">${itemData.rating}</p>
+    <p class="gamePrice">${itemData.price - itemData.price * itemData.discount}</p>
+    <p class="gamePrice">${itemData.releaseDate}</p>
+    <button id="${itemData.title}" class= "buyBtn">Add to Cart</button>
+`
+
+let result = []
+for (let i = 0; i < itemData.platforms.length; i++) {
+    result.push(" " + itemData.platforms[i] + " ")  
+}
+document.querySelector(".gamePlatforms").innerHTML += result.join(" | ")
+
+addEventListenerToBtns();
