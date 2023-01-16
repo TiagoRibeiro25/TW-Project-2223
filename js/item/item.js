ff5a19a86df01d27d40a37710dda07c43239953b
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2023-01-16 16:22:57 +0000

    improved code organization

--- js/item/item.js
+++ js/item/item.js
@@ -10,20 +10,20 @@ const itemData = catalogData.find((item) => item.title === itemName);
 // if item not found, redirect to 404 page
 if (!itemData) window.location.href = "../html/404.html";
 
-consolse.log(itemData); // Objeto com os dados do item/jogo
+console.log(itemData); // Objeto com os dados do item/jogo
 document.querySelector("h1").innerHTML = itemData.title;
 document.querySelector("img").src = itemData.image;
 document.querySelector(".gameDescription").innerHTML = itemData.description;
 document.querySelector(".gameDetails").innerHTML += `
-    <p class="gameSpec">Rating: ${itemData.rating}</p>
-    <br>
-    <p class="gameSpecs">Price: ${
-      itemData.price - itemData.price * itemData.discount
-    } €</p>
-    <br>
-    <p class="gameSpecs">Release Date: ${itemData.releaseDate}</p>
-    <br>
-    <button id="${itemData.title}" class= "buyBtn">Add to Cart</button>
+  <p class="gameSpec">Rating: ${itemData.rating}</p>
+  <br>
+  <p class="gameSpecs">Price: ${
+    itemData.price - itemData.price * itemData.discount
+  } €</p>
+  <br>
+  <p class="gameSpecs">Release Date: ${itemData.releaseDate}</p>
+  <br>
+  <button id="${itemData.title}" class= "buyBtn">Add to Cart</button>
 `;
 
 let result = [];
