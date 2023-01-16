41a98cf896a35f5e3e814ef8129dc46dfc7c9954
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2023-01-16 16:21:57 +0000

    improved code organization

--- js/item/item.js
+++ js/item/item.js
@@ -1,7 +1,6 @@
 import { addEventListenerToBtns } from "../catalog/addToCart.js";
 import { catalogData } from "../getData.js";
 
-
 // get item name from the url
 const urlParams = new URLSearchParams(window.location.search);
 const itemName = urlParams.get("title");
@@ -11,24 +10,28 @@ const itemData = catalogData.find((item) => item.title === itemName);
 // if item not found, redirect to 404 page
 if (!itemData) window.location.href = "../html/404.html";
 
-console.log(itemData); // Objeto com os dados do item/jogo
-document.querySelector("h1").innerHTML = itemData.title
-document.querySelector("img").src = itemData.image
-document.querySelector(".gameDescription").innerHTML = itemData.description
+consolse.log(itemData); // Objeto com os dados do item/jogo
+document.querySelector("h1").innerHTML = itemData.title;
+document.querySelector("img").src = itemData.image;
+document.querySelector(".gameDescription").innerHTML = itemData.description;
 document.querySelector(".gameDetails").innerHTML += `
-    <p class="gameSpecs">Rating: ${itemData.rating}</p>
+    <p class="gameSpec">Rating: ${itemData.rating}</p>
     <br>
-    <p class="gameSpecs">Price: ${itemData.price - itemData.price * itemData.discount}</p>
+    <p class="gameSpecs">Price: ${
+      itemData.price - itemData.price * itemData.discount
+    } €</p>
     <br>
     <p class="gameSpecs">Release Date: ${itemData.releaseDate}</p>
     <br>
     <button id="${itemData.title}" class= "buyBtn">Add to Cart</button>
-`
+`;
 
-let result = []
-for (let i = 0; i < itemData.platforms.length; i++) {
-    result.push(" " + itemData.platforms[i] + " ")  
+let result = [];
+
+for (const platform of itemData.platforms) {
+  result.push(` ${platform} `);
 }
-document.querySelector(".gamePlatforms").innerHTML += result.join(" | ")
+
+document.querySelector(".gamePlatforms").innerHTML += result.join(" | ");
 
 addEventListenerToBtns();
